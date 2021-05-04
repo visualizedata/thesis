a8422e9b9263c656675a4927e1674583a827a428
papermashea <molloy.shea@gmail.com>
2021-05-04 12:32:10 -0400

    correct video opens full screen, tap to close

--- test.js
+++ test.js
@@ -1,9 +1,23 @@
 // SHOWS ALL VIDEO
 
 $('.video').click(function(){
-      $('.video-player').toggleClass('show');
+      $("div[id=" + $(this).attr("data-related") + "]").toggleClass('show');
 });
 
 // $('.video').click(function(){
 //       $(this).next('div').toggle();
 // });
+
+
+
+// FULL SCREEN
+// var elem = document.getElementById("myvideo");
+// if (elem.requestFullscreen) {
+//   elem.requestFullscreen();
+// } else if (elem.mozRequestFullScreen) {
+//   elem.mozRequestFullScreen();
+// } else if (elem.webkitRequestFullscreen) {
+//   elem.webkitRequestFullscreen();
+// } else if (elem.msRequestFullscreen) { 
+//   elem.msRequestFullscreen();
+// }
