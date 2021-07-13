9bdf037b1fcef7fafc39434c13f5f179319785b3
Alec Barrett <alec.barrett@datadoghq.com>
2021-07-12 21:57:47 -0400

    initial inner repo + simple grid layout

---
+++ thesis/src/constants.js
@@ -0,0 +1,2 @@
+export const HEIGHT_TO_WIDTH = 0.5625;
+export const BODY_WIDTH = 0.8;
---
+++ thesis/src/views/Home.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="home">
+    <Header />
+    <Projects :height="height" :width="width" />
+    <Footer />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Header from "@/components/Header.vue";
+import Footer from "@/components/Footer.vue";
+import Projects from "@/components/Projects.vue";
+
+export default {
+  name: "Home",
+  components: {
+    Header,
+    Footer,
+    Projects,
+  },
+  data() {
+    return {
+      height: 0,
+      width: 0,
+    };
+  },
+  mounted() {
+    window.addEventListener("resize", this.onResize);
+    this.onResize();
+  },
+  unmounted() {
+    window.removeEventListener("resize", this.onResize);
+  },
+  methods: {
+    onResize() {
+      this.height = window.innerHeight;
+      this.width = window.innerWidth;
+    },
+  },
+};
+</script>
