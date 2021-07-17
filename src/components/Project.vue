15acc98031e230c07b3de4681d44c11e4c7d1131
Alec Barrett <alec.barrett@datadoghq.com>
2021-07-16 22:08:01 -0400

    major file reorg

---
+++ src/components/Project.vue
@@ -0,0 +1,49 @@
+<template>
+  <el-card class="project" :style="cellStyle">
+    {{ name }} {{ title }}
+
+    <img :src="require(`@/assets/images/${image}`)" />
+  </el-card>
+</template>
+
+<script>
+import { HEIGHT_TO_WIDTH } from "@/constants.js";
+
+export default {
+  name: "Project",
+  props: {
+    name: String,
+    title: String,
+    description: String,
+    image: String,
+    repo: String,
+    video: String,
+    index: Number,
+    position: Object,
+    width: Number,
+  },
+  computed: {
+    height() {
+      return this.width * HEIGHT_TO_WIDTH;
+    },
+    cellStyle() {
+      return {
+        top: `${this.position.y}px`,
+        left: `${this.position.x}px`,
+        width: `${this.width}px`,
+        height: `${this.height}px`,
+      };
+    },
+  },
+  methods: {},
+};
+</script>
+
+<style scoped>
+.project {
+  position: absolute;
+}
+.project img {
+  width: 100%;
+}
+</style>
