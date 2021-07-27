d9011c1f7274da91cae9164e1ae19b64cbce44ce
Alec Barrett <alec.barrett@datadoghq.com>
2021-07-27 09:18:28 -0400

    add year filter

--- src/components/Filters.vue
+++ src/components/Filters.vue
@@ -1,59 +1,116 @@
 <template>
   <div class="filters">
-    <el-select
-      class="tag-filter"
-      multiple
-      v-model="filterTag.selected"
-      @change="(options) => onFilterChange('TAG', options)"
-      placeholder="Filter by tag(s)"
-    >
-      <el-option
-        v-for="item in filterTag.options"
-        :key="item"
-        :label="item"
-        :value="item"
-      />
-    </el-select>
-    <el-input
-      class="search-filter"
-      placeholder="Search projects"
-      clearable
-      v-model="filterSearch.selected"
-      @change="(text) => onFilterChange('SEARCH', text)"
-    />
-    <div>
-      <el-select
-        class="sort-param"
-        v-model="paramSort.selected"
-        @change="(options) => onFilterChange('SORT', options)"
-      >
-        <el-option
-          v-for="item in paramSort.options"
-          :key="item"
-          :label="item.label"
-          :value="item.value"
+    <el-row>
+      <div class="filter tag-filter">
+        <div class="label">Filter by tag</div>
+        <el-select
+          multiple
+          v-model="filterTag.selected"
+          @change="(options) => onFilterChange('TAG', options)"
+          placeholder="Select project tag(s)"
         >
-        </el-option>
-      </el-select>
-      <el-button
-        class="sort-button"
-        type="text"
-        :icon="sortIcon"
-        @click="() => onFilterChange('SORT_DIRECTION', null)"
-      ></el-button>
-    </div>
-    <div class="project-count">Showing {{ nProjects }} projects</div>
+          <el-option
+            v-for="item in filterTag.options"
+            :key="item"
+            :label="item"
+            :value="item"
+          />
+        </el-select>
+      </div>
+      <div class="filter search-filter">
+        <div class="label">Filter on text</div>
+        <el-input
+          placeholder="Search name, title, or description"
+          clearable
+          v-model="filterSearch.selected"
+          @change="(text) => onFilterChange('SEARCH', text)"
+        />
+      </div>
+      <div class="filter year-filter">
+        <div class="label">Filter by year</div>
+        <svg :height="`${filterHeight}px`" width="200px">
+          <g class="year-bars-all">
+            <rect
+              v-for="bar in yearData.all"
+              :key="`all_${bar[0]}`"
+              :x="xScale(bar[0])"
+              :y="filterHeight - yScale(bar[1])"
+              :height="yScale(bar[1])"
+              :width="xScale.bandwidth()"
+            />
+          </g>
+          <g class="year-bars-filtered">
+            <rect
+              v-for="bar in yearData.filtered"
+              :key="`all_${bar[0]}`"
+              :x="xScale(bar[0])"
+              :y="filterHeight - yScale(bar[1])"
+              :height="yScale(bar[1])"
+              :width="xScale.bandwidth()"
+            />
+          </g>
+          <g class="year-labels">
+            <text
+              v-for="text in yearData.all"
+              :key="text[0]"
+              :x="xScale(text[0]) + xScale.bandwidth() / 2"
+              :y="filterHeight - 3"
+            >
+              {{ text[0] }}
+            </text>
+          </g>
+        </svg>
+      </div>
+      <div class="filter sort-param">
+        <div class="label">Sort</div>
+        <div class="sort-param-content">
+          <el-select
+            v-model="paramSort.selected"
+            @change="(options) => onFilterChange('SORT', options)"
+          >
+            <el-option
+              v-for="item in paramSort.options"
+              :key="item"
+              :label="item.label"
+              :value="item.value"
+            >
+            </el-option>
+          </el-select>
+          <el-button
+            class="sort-button"
+            type="text"
+            :icon="sortIcon"
+            @click="() => onFilterChange('SORT_DIRECTION', null)"
+          ></el-button>
+        </div>
+      </div>
+    </el-row>
+    <el-row>
+      <div class="filter project-count">
+        Showing {{ nProjects.filtered }} of {{ nProjects.all }} projects
+      </div>
+    </el-row>
   </div>
 </template>
 
 <script>
+import { scaleLinear, scaleBand } from "d3-scale";
+import { max } from "d3-array";
+const FILTER_HEIGHT = 40;
+
 export default {
   name: "Filters",
   props: {
     filters: Object,
     params: Object,
     onFilterChange: Function,
-    nProjects: Number,
+    nProjects: Object,
+    yearData: Object,
+  },
+  data() {
+    return {
+      filterHeight: FILTER_HEIGHT,
+    };
   },
   computed: {
     filterTag() {
@@ -71,6 +128,18 @@ export default {
       }
       return "el-icon-sort-up";
     },
+    xScale() {
+      return scaleBand()
+        .domain(this.yearData.all.map((d) => d[0]))
+        .range([0, 200])
+        .paddingOuter(0.2)
+        .paddingInner(0.1);
+    },
+    yScale() {
+      return scaleLinear()
+        .domain([0, max(this.yearData.all, (d) => d[1])])
+        .range([0, this.filterHeight]);
+    },
   },
 };
 </script>
@@ -79,10 +148,22 @@ export default {
 .filters {
   width: 80%;
   margin: 20px 10%;
+  font-family: "neue-regular";
+}
+.filters .el-row {
   display: flex;
   flex-direction: row;
   justify-content: space-between;
-  font-family: "neue-regular";
+}
+
+.label {
+  font-size: 11px;
+  text-align: left;
+  padding-bottom: 5px;
+}
+
+.filter {
+  margin-top: 10px;
 }
 
 .sort-button {
@@ -90,19 +171,40 @@ export default {
 }
 
 .search-filter {
-  max-width: 400px;
+  width: 260px;
+}
+.search-filter input {
+  width: 100%;
 }
 
 .tag-filter {
   min-width: 300px;
 }
+.tag-filter .el-select {
+  width: 100%;
+}
 
 .sort-param {
-  width: 140px;
+  width: 160px;
+}
+.sort-param-content {
+  display: flex;
 }
 
 .project-count {
-  min-width: 200px;
-  line-height: 40px;
+  font-size: 12px;
+}
+
+.year-bars-all rect {
+  fill: gray;
+}
+.year-bars-filtered rect {
+  fill: black;
+  transition: all 0.5s;
+}
+.year-labels text {
+  fill: white;
+  font-size: 9px;
+  text-anchor: middle;
 }
 </style>
